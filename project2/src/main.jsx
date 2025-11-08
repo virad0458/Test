eb90173e9e2808dabb0c9ba2e351390733fdfda2
virad0458 <virad0458@gmail.com>
2025-11-08 08:06:59 +0000

    images

--- project2/src/main.jsx
+++ project2/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.js'
+import App from './App.jsx'
 import './index.css'   // <--- make sure this is here
 
 ReactDOM.createRoot(document.getElementById('root')).render(
